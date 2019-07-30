6078225265337ea0283d0b453bceae79f95fcad7
Ransel <ranselc@gmail.com>
2019-07-29 22:56:09 -0400

    Adding Initial SeedDb

--- Shop.Web/Program.cs
+++ Shop.Web/Program.cs
@@ -1,24 +1,30 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore;
+﻿using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.DependencyInjection;
+using Shop.Web;
+using Shop.Web.Data;
 
-namespace Shop.Web
+public class Program
 {
-    public class Program
+    public static void Main(string[] args)
     {
-        public static void Main(string[] args)
-        {
-            CreateWebHostBuilder(args).Build().Run();
-        }
-
-        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
-            WebHost.CreateDefaultBuilder(args)
-                .UseStartup<Startup>();
+        var host = CreateWebHostBuilder(args).Build();
+        RunSeeding(host);
+        host.Run();
     }
+
+    private static void RunSeeding(IWebHost host)
+    {
+        var scopeFactory = host.Services.GetService<IServiceScopeFactory>();
+        using (var scope = scopeFactory.CreateScope())
+        {
+            var seeder = scope.ServiceProvider.GetService<SeedDb>();
+            seeder.SeedAsync().Wait();
+        }
+    }
+
+    public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+        WebHost.CreateDefaultBuilder(args)
+            .UseStartup<Startup>();
 }
+
--- Shop.Web/Startup.cs
+++ Shop.Web/Startup.cs
@@ -32,6 +32,7 @@ namespace Shop.Web
                 cfg.UseSqlServer(Configuration.GetConnectionString("DefaultConnection"));
             });
 
+            services.AddTransient<SeedDb>();
 
             services.Configure<CookiePolicyOptions>(options =>
             {
